4b8cacff56ed30c196c2c330961bdcefb475fd30
nikolasmelui <slak@samaradom.ru>
2020-08-05 10:44:28 +0400

    Initial commit

---
+++ src/main/java/beer4/social/SocialApplication.java
@@ -0,0 +1,13 @@
+package beer4.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialApplication.class, args);
+	}
+
+}
