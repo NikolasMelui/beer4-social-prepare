26134614725ae3350638f84a4b1b0c2cbfe65240
nikolasmelui <slak@samaradom.ru>
2020-08-05 19:03:38 +0400

    Update the Message controller with the hibernate logic

--- src/main/java/beer4/social/controller/MessageController.java
+++ src/main/java/beer4/social/controller/MessageController.java
@@ -1,54 +1,46 @@
 package beer4.social.controller;
 
-import beer4.social.exceptions.NotFoundException;
+import beer4.social.domain.Message;
+import beer4.social.repository.MessageRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("message")
 public class MessageController {
-    private int counter = 5;
+    private final MessageRepository messageRepository;
 
-    public List<Map<String, String>> messages = new ArrayList<Map<String, String>>(){{
-        add(new HashMap<String, String>() {{ put("id", "1"); put("text", "First message"); }});
-        add(new HashMap<String, String>() {{ put("id", "2"); put("text", "Second message"); }});
-        add(new HashMap<String, String>() {{ put("id", "3"); put("text", "Third message"); }});
-        add(new HashMap<String, String>() {{ put("id", "4"); put("text", "Fourth message"); }});
-    }};
+    @Autowired
+    public MessageController(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
 
     @GetMapping()
-    public List<Map<String, String>> getMessages() {
-        return messages;
+    public List<Message> getMessages() {
+        return messageRepository.findAll();
     }
 
     @GetMapping("{id}")
-    public Map<String, String> getMessageById (@PathVariable String id) {
-        return messages.stream().filter(message -> message.get("id").equals(id)).findFirst().orElseThrow(NotFoundException::new);
-    }
-
-    @PostMapping()
-    public Map<String, String> createMessage(@RequestBody Map<String, String> message) {
-        message.put("id", String.valueOf(counter++));
-        messages.add(message);
+    public Message getMessageById(@PathVariable("id") Message message) {
         return message;
     }
 
+    @PostMapping()
+    public Message createMessage(@RequestBody Message message) {
+        return messageRepository.save(message);
+    }
+
     @PutMapping("{id}")
-    public Map<String, String> updateMessage(@PathVariable String id, @RequestBody Map<String, String> message) {
-        Map<String, String> currentMessage = getMessageById(id);
-        currentMessage.putAll(message);
-        currentMessage.put("id", id);
-        return currentMessage;
+    public Message updateMessage(@PathVariable("id") Message messageFromDB, @RequestBody Message message) {
+        BeanUtils.copyProperties(message, messageFromDB, "id");
+        return messageRepository.save(messageFromDB);
     }
 
     @DeleteMapping("{id}")
-    public int deleteMessage(@PathVariable String id) {
-        Map<String, String> message = getMessageById(id);
-        messages.remove(message);
-        return 1;
+    public void deleteMessage(@PathVariable("id") Message message) {
+        messageRepository.delete(message);
     }
 }
